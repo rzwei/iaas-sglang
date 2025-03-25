5e4cc34bd8d6929ca438274fb87a3fa092a6d20a
Ho-Ren (Jack) Chuang <horenchuang@bytedance.com>
2025-03-25 05:16:18 +0000

    [Triton][Misc] Add SGL_USE_TRITON_NON_ATTN env variable and triton path in CustomOp
    Add an environmental variable to control whether or not to use triton operators.
    
    Add a forward_triton path in Custom for Triton kernel implementation.
    
    Signed-off-by: Ho-Ren (Jack) Chuang <horenchuang@bytedance.com>
    Co-authored-by: Tony Zhang <tony.zhang@bytedance.com>

--- python/sglang/srt/custom_op.py
+++ python/sglang/srt/custom_op.py
@@ -3,6 +3,8 @@ from typing import Optional
 import torch
 from torch import nn
 
+import os
+
 from sglang.srt.utils import is_cuda, is_hip
 
 _is_cuda = is_cuda()
@@ -23,6 +25,9 @@ class CustomOp(nn.Module):
     def forward_cuda(self, *args, **kwargs):
         raise NotImplementedError
 
+    def forward_triton(self, *args, **kwargs):
+        raise NotImplementedError
+
     def forward_hip(self, *args, **kwargs):
         return self.forward_cuda(*args, **kwargs)
 
@@ -35,7 +40,13 @@ class CustomOp(nn.Module):
     def forward_cpu(self, *args, **kwargs):
         return self.forward_native(*args, **kwargs)
 
+    def forward_triton(self, *args, **kwargs):
+        return self.forward_triton(*args, **kwargs)
+
     def dispatch_forward(self):
+        if os.environ.get("SGL_USE_TRITON_NON_ATTN", "").lower() in ("true", "1"):
+            return self.forward_triton
+
         if _is_cuda:
             return self.forward_cuda
         elif _is_hip:
