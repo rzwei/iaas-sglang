bf3f4535decbd146a15987c0397b9e643b0535ed
Your Name <you@example.com>
2025-04-14 15:07:44 +0000

    ep torch compile and config

--- python/sglang/srt/layers/moe/ep_moe/layer.py
+++ python/sglang/srt/layers/moe/ep_moe/layer.py
@@ -4,6 +4,9 @@ from typing import Callable, List, Optional, Tuple
 import torch
 from torch.nn import Module
 
+import torch._dynamo
+torch._dynamo.config.suppress_errors = True
+
 try:
     from deep_gemm import (
         get_col_major_tma_aligned_tensor,
