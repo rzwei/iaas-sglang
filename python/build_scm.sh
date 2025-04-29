ca580018c0eb4286202951f4a6ed348440d798c4
ShawnKung <42027195+ShawnKung@users.noreply.github.com>
2025-04-29 11:55:57 +0800

    build(scm): tos bucket name from env (#24)
    * build(scm): tos bucket name from env
    
    * build(scm): add release branch name format pkg version
    
    ---------
    
    Co-authored-by: Yuxiang Gong <gongyuxiang@bytedance.com>

--- python/build_scm.sh
+++ python/build_scm.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-set -ex
+set -e
 
 ROOT_DIR=$(pwd)
 PYTHON=python3
 PIP="$PYTHON -m pip"
 BUILD_TIME=$(date +%Y%m%d%H%M)
-VERSION_SUFFIX=+byted.${BUILD_TIME}
+# 获取当前分支名，并将特殊字符转换为下划线
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+echo "BRANCH_NAME: $BRANCH_NAME"
+
+# 如果分支是以 release_ 或 release/ 开头，则将 release_ 或 release/ 替换为空
+if [[ $BRANCH_NAME =~ ^release[\/_] ]]; then
+    BRANCH_NAME=${BRANCH_NAME#release}
+    BRANCH_NAME=${BRANCH_NAME#/}
+    BRANCH_NAME=${BRANCH_NAME#_}
+    # 如果分支里还有 / ，则将 / 替换为 .
+    BRANCH_NAME=${BRANCH_NAME//\//.}
+    if [[ ! -z $BRANCH_NAME ]]; then
+        BRANCH_NAME=.${BRANCH_NAME}
+    fi
+    VERSION_SUFFIX=+byted${BRANCH_NAME}.${BUILD_TIME}
+else
+    VERSION_SUFFIX=+byted.${BUILD_TIME}
+fi
+
+echo "VERSION_SUFFIX: $VERSION_SUFFIX"
 
 TOS_UTIL_URL=https://tos-tools.tos-cn-beijing.volces.com/linux/amd64/tosutil
 if [ ! -z "$CUSTOM_TOS_UTIL_URL" ]; then
@@ -37,6 +56,6 @@ else
     for wheel_file in $(find $OUTPUT_DIR -name "*.whl"); do
         echo "Uploading to tos"
         # 上传制品到 tos
-        ./tosutil cp $wheel_file tos://iaas-pypi/packages/sglang/$(basename $wheel_file) -re cn-beijing -e tos-cn-beijing.volces.com -i $CUSTOM_TOS_AK -k $CUSTOM_TOS_SK
+        ./tosutil cp $wheel_file tos://${CUSTOM_TOS_BUCKET}/packages/sglang/$(basename $wheel_file) -re cn-beijing -e tos-cn-beijing.volces.com -i $CUSTOM_TOS_AK -k $CUSTOM_TOS_SK
     done
 fi
