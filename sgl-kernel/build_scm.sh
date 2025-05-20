8ca1723d3e4713184ffbd8d35610e39479aaa783
ShawnKung <42027195+ShawnKung@users.noreply.github.com>
2025-05-20 12:03:04 +0800

    build(scm): dind build support (#44)
    Co-authored-by: Yuxiang Gong <gongyuxiang@bytedance.com>

--- sgl-kernel/build_scm.sh
+++ sgl-kernel/build_scm.sh
@@ -20,10 +20,11 @@ if [ -z "$CUDA_VERSION" ]; then
     CUDA_VERSION="12.4"
 fi
 
-PYTHON_ROOT_PATH=/opt/python/cp${PYTHON_VERSION//.}-cp${PYTHON_VERSION//.}
-
 ROOT_PATH=$(pwd)
 OUTPUT_PATH=$ROOT_PATH/output
+mkdir -p $OUTPUT_PATH
+cd sgl-kernel
+
 # 获取当前分支名，并将特殊字符转换为下划线
 BUILD_TIME=$(date +%Y%m%d%H%M)
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
@@ -50,60 +51,38 @@ echo "VERSION_SUFFIX: $VERSION_SUFFIX"
 
 ENABLE_SM90A=$(( ${CUDA_VERSION%.*} >= 12 ? ON : OFF ))
 
-if [ ${CUDA_VERSION} = "12.8" ]; then
-   DOCKER_IMAGE="iaas-gpu-cn-beijing.cr.volces.com/pytorch/manylinux2_28-builder:cuda${CUDA_VERSION}"
-   TORCH_INSTALL="pip install --no-cache-dir --pre torch --index-url https://download.pytorch.org/whl/nightly/cu${CUDA_VERSION//.}"
-else
-   DOCKER_IMAGE="iaas-gpu-cn-beijing.cr.volces.com/pytorch/manylinux-builder:cuda${CUDA_VERSION}"
-   TORCH_INSTALL="pip install --no-cache-dir torch==2.5.1 --index-url https://download.pytorch.org/whl/cu${CUDA_VERSION//.}"
+VERSION=$(sed -n 's/^version = "\([^"]*\)"/\1/p' pyproject.toml)
+# 移除可能存在的 byted 后缀，获取基础版本号
+BASE_VERSION=$(echo $VERSION | sed 's/+byted.*$//')
+echo "Building sglang-python version $BASE_VERSION$VERSION_SUFFIX"
+sed -i "s/^version = .*$/version = \"$BASE_VERSION$VERSION_SUFFIX\"/" pyproject.toml
+
+proxy_args=""
+if [ ! -z "$http_proxy" ]; then
+    proxy_args="$proxy_args -e http_proxy=$http_proxy"
+fi
+if [ ! -z "$https_proxy" ]; then
+    proxy_args="$proxy_args -e https_proxy=$https_proxy"
 fi
 
-# 由于 SCM 的特殊配置，挂载了物理机上的路径到容器内的 /sgl_kernel 目录，所以这里需要拷贝源码到 /sgl_kernel 目录下
-BASE_PATH=/sgl_kernel
-SRC_PATH=$BASE_PATH/sgl-kernel && mkdir -p $SRC_PATH
+sed -i "s|docker run --rm|docker run --rm --network=host $proxy_args|" build.sh
+sed -i "s|pytorch/manylinux|iaas-gpu-cn-beijing.cr.volces.com/pytorch/manylinux|g" build.sh
+sed -i 's|ARCH=$(uname -i)|ARCH=x86_64|g' build.sh  # DinD 可能不支持 ARCH=$(uname -i)
 
-rm -rf $SRC_PATH/* && cp -r $(pwd)/sgl-kernel/* $SRC_PATH/
-cd $SRC_PATH
-VERSION=$(sed -n 's/^version = "\([^"]*\)"/\1/p' pyproject.toml)
-echo "Building sglang-python version $VERSION$VERSION_SUFFIX"
-sed -i "s/^version = .*$/version = \"$VERSION$VERSION_SUFFIX\"/" pyproject.toml
+# 如果是 SCM 构建，则准备 docker 环境
+if [ $SCM_BUILD == "True" ]; then
+    dockerd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock &
+    while ! docker info > /dev/null 2>&1; do
+        sleep 1
+        echo "Waiting for dockerd to start..."
+    done
+    export DOCKER_HOST="tcp://localhost:2375"
+fi
 
-docker run --rm \
-   -v $(pwd):/sgl-kernel \
-   --network=host \
-   -e http_proxy=$http_proxy \
-   -e https_proxy=$https_proxy \
-   ${DOCKER_IMAGE} \
-   bash -c "
-   # Install CMake (version >= 3.26) - Robust Installation
-   export CMAKE_VERSION_MAJOR=3.31
-   export CMAKE_VERSION_MINOR=1
-   echo \"Downloading CMake from: https://cmake.org/files/v\${CMAKE_VERSION_MAJOR}/cmake-\${CMAKE_VERSION_MAJOR}.\${CMAKE_VERSION_MINOR}-linux-x86_64.tar.gz\"
-   wget https://cmake.org/files/v\${CMAKE_VERSION_MAJOR}/cmake-\${CMAKE_VERSION_MAJOR}.\${CMAKE_VERSION_MINOR}-linux-x86_64.tar.gz
-   tar -xzf cmake-\${CMAKE_VERSION_MAJOR}.\${CMAKE_VERSION_MINOR}-linux-x86_64.tar.gz
-   mv cmake-\${CMAKE_VERSION_MAJOR}.\${CMAKE_VERSION_MINOR}-linux-x86_64 /opt/cmake
-   export PATH=/opt/cmake/bin:\$PATH
-
-   # Debugging CMake
-   echo \"PATH: \$PATH\"
-   which cmake
-   cmake --version
-
-   ${PYTHON_ROOT_PATH}/bin/${TORCH_INSTALL} && \
-   ${PYTHON_ROOT_PATH}/bin/pip install --no-cache-dir ninja setuptools==75.0.0 wheel==0.41.0 numpy uv scikit-build-core && \
-   export TORCH_CUDA_ARCH_LIST='7.5 8.0 8.9 9.0+PTX' && \
-   export CUDA_VERSION=${CUDA_VERSION} && \
-   mkdir -p /usr/lib/x86_64-linux-gnu/ && \
-   ln -s /usr/local/cuda-${CUDA_VERSION}/targets/x86_64-linux/lib/stubs/libcuda.so /usr/lib/x86_64-linux-gnu/libcuda.so && \
-   cd /sgl-kernel && \
-   ls -la ${PYTHON_ROOT_PATH}/lib/python${PYTHON_VERSION}/site-packages/wheel/ && \
-   PYTHONPATH=${PYTHON_ROOT_PATH}/lib/python${PYTHON_VERSION}/site-packages ${PYTHON_ROOT_PATH}/bin/python -m uv build --wheel -Cbuild-dir=build . --color=always --no-build-isolation -v && \
-   ./rename_wheels.sh
-   "
+source build.sh $PYTHON_VERSION $CUDA_VERSION
 
 # 产物放到 output 目录下
-mkdir -p $OUTPUT_PATH
-cp -r $SRC_PATH/dist/* $OUTPUT_PATH/
+cp -r $ROOT_PATH/sgl-kernel/dist/* $OUTPUT_PATH/
 
 TOS_UTIL_URL=https://tos-tools.tos-cn-beijing.volces.com/linux/amd64/tosutil
 if [ ! -z "$CUSTOM_TOS_UTIL_URL" ]; then
